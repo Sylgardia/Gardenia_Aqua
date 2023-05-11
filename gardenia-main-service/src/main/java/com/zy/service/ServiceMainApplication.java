d6b3d5bffa54eab2150212e2ff642e02cb9d91b2
ZhangYu <2729045223@qq.com>
2023-05-11 15:07:27 +0800

    PyTorch 集成 深度学习！

--- gardenia-main-service/src/main/java/com/zy/service/ServiceMainApplication.java
+++ gardenia-main-service/src/main/java/com/zy/service/ServiceMainApplication.java
@@ -7,23 +7,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * @author zhangyu
  */
 @Slf4j
 @SpringBootApplication
 public class ServiceMainApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         ConfigurableApplicationContext application =  SpringApplication.run(ServiceMainApplication.class, args);
         Environment env = application.getEnvironment();
-//        String ip = InetAddress.getLocalHost().getHostAddress();
+        String ip = InetAddress.getLocalHost().getHostAddress();
         String port = env.getProperty("server.port");
         String path = ConvertUtils.getString(env.getProperty("server.servlet.context-path"));
         log.info("\n----------------------------------------------------------\n\t" +
                 "Application Jeecg-Boot is running! Access URLs:\n\t" +
                 "Local: \t\thttp://localhost:" + port + path + "/doc.html\n" +
-//                "External: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
-//                "文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "External: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
                 "----------------------------------------------------------");
     }
 }
